9f44641ab9eb0e2ee3f165c62745bb10dda331fa
Dennis Riungu <denpalrius@gmail.com>
2018-11-19 21:45:32 +0300

    Added material modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,51 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NewActivityComponent } from './components/new-activity/new-activity.component';
+import {
+  MatInputModule,
+  MatButtonModule,
+  MatSelectModule,
+  MatCardModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatListModule,
+  MatGridListModule,
+  MatMenuModule,
+  MatDatepickerModule,
+  MatRadioModule,
+  MatSnackBarModule,
+} from '@angular/material';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ProgramsListComponent } from './components/programs-list/programs-list.component';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewActivityComponent,
+    ProgramsListComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatDatepickerModule,
+    MatRadioModule,
+    MatSnackBarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
