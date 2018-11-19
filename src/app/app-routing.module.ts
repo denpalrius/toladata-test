a1737400671fa6e1adc916bba3856d3f53be133d
Dennis Riungu <denpalrius@gmail.com>
2018-11-19 21:45:47 +0300

    Configured app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NewActivityComponent } from './components/new-activity/new-activity.component';
+import { ProgramsListComponent } from './components/programs-list/programs-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProgramsListComponent },
+  { path: 'new-activity', component: NewActivityComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { useHash: false })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
